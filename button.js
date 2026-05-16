e7ad1bc564d10c0da548d85772bb64cd17af62fd
Jessica <eugenieekazi@gmail.com>
2026-05-16 12:28:06 +0200

    debugged the errors

--- button.js
+++ button.js
@@ -1,58 +1,50 @@
 const displayView = document.getElementById("calculatorScreen");
 
-function appendToDisplay(input){
-    displayView.value += input
+function appendToDisplay(input) {
+    displayView.value += input;
 }
 
-function clearSum () {
-  displayView.value = ""
+function clearSum() {
+    displayView.value = "";
 }
 
-function equalCal () {
-  
+function equalCal() {
+    displayView.value = calculateTheSum();
 }
 
 function calculateTheSum() {
-  // the regex keeps the operate after the spilt removes them
-  const SumOperator = displayView.value.split(/(\^2|[+\-*/√^]+)/); 
-  let sum = parseFloat(SumOperator[0]);
+    const SumOperator = displayView.value.split(/(\^2|[+\-*/√^])/);
 
-  for (let i = 1; i < SumOperator.length; i+=2 ) {
-    const operators = SumOperator[i];
-    const secondSum = parsefloat(SumOperator[i+1]);
+    let sum = parseFloat(SumOperator[0]);
 
-    if (SumOperator === '+') {
-      sum += secondSum;  
-    } 
-    else if (SumOperator === "-") {
-      sum -= secondSum;
-    } 
-    else if (SumOperator === "*") {
-      sum *= secondSum;
-    } 
-    else if (SumOperator === "/") {
-      if (secondSum == 0) {
-        return displayView.value="error"
-      } 
-      else {
-        sum /= secondSum;
-      }
-    } 
-    else if (SumOperator === "^") {
-      sum = Math.pow(sum, secondSum);
-    } 
-    else if (SumOperator === "^2") {
-      sum = Math.pow(secondSum, 2);
-    } 
-    else if (SumOperator === "√") {
-      if (isNaN(sum)) {
-        sum = Math.sqrt(secondSum)
-      } 
-      else {
-        sum *= Math.sqrt(secondSum)
-      }
+    for (let i = 1; i < SumOperator.length; i += 2) {
+        const operators = SumOperator[i];
+        const secondSum = parseFloat(SumOperator[i + 1]);
+
+        if (operators === "+") {
+            sum += secondSum;
+        }
+        else if (operators === "-") {
+            sum -= secondSum;
+        }
+        else if (operators === "*") {
+            sum *= secondSum;
+        }
+        else if (operators === "/") {
+            if (secondSum === 0) {
+                return "Error";
+            }
+            sum /= secondSum;
+        }
+        else if (operators === "^") {
+            sum = Math.pow(sum, secondSum);
+        }
+        else if (operators === "^2") {
+            sum = Math.pow(sum, 2);
+        }
+        else if (operators === "√") {
+            sum = Math.sqrt(secondSum);
+        }
     }
-  }
-  displayView.value = sum
+    return sum;
 }
-
